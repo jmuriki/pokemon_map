162bb6b43f600575071dea3de12c86a3c8f1139f
jmuriki <106560016+jmuriki@users.noreply.github.com>
2022-12-21 20:16:56 +0300

    Add translations to admin page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,13 +2,29 @@ from django.db import models  # noqa F401
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
-    description = models.TextField(null=True)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
+    title = models.CharField(
+        verbose_name="Название на русском",
+        max_length=200,
+    )
+    image = models.ImageField(
+        verbose_name="Изображение",
+        null=True,
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        null=True,
+    )
+    title_en = models.CharField(
+        verbose_name="Название на английском",
+        max_length=200,
+    )
+    title_jp = models.CharField(
+        verbose_name="Название на японском",
+        max_length=200,
+    )
     evolved_from = models.ForeignKey(
                         "self",
+                        verbose_name="Эволюционный предшественник",
                         null=True,
                         blank=True,
                         on_delete=models.SET_NULL,
@@ -20,13 +36,17 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name="Покемон",
+        on_delete=models.CASCADE,
+    )
+    lat = models.FloatField(verbose_name="Широта местонахождения")
+    lon = models.FloatField(verbose_name="Долгота местонахождения")
+    appeared_at = models.DateTimeField(verbose_name="Дата появления")
+    disappeared_at = models.DateTimeField(verbose_name="Дата исчезновения")
+    level = models.IntegerField(verbose_name="Уровень")
+    health = models.IntegerField(verbose_name="Здоровье")
+    strength = models.IntegerField(verbose_name="Атака")
+    defence = models.IntegerField(verbose_name="Защита")
+    stamina = models.IntegerField(verbose_name="Выносливость")
